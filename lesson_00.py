8e6b6aa01238aeca664ae10f29d594cfaa2198db
Selah Hilaire <shilaire@brooklynsteamcenter.org>
2024-09-16 10:44:34 -0400

    MAke changes

---
+++ lesson_00.py
@@ -0,0 +1,20 @@
+#Step one (i love macaroni and cheese)
+print('Selah')
+#step dos (I love Alfredo)
+fav_show = "YOU"
+#step 3 (Noel loves elbow macaroni)
+float()
+#step fo!(Neezy loves fettuccini)
+num1 = 2
+num2 = 3
+product = num1 * num2
+#step five(I love ravioli)
+fave_foods =("i love macaroni and cheese,Noel loves elbow macaroni,I love Alfredo")
+#step sies (Sam loves spaghetti and meatballs)
+print("can you get a drivers license")
+#step 7/11(Kyle Chicken Alfredo)
+name=input("Tell me your name...")
+print("your name is ",name)
+#step 4+4 (I feel like I will stop here)
+print("T-T")
+while(""):
